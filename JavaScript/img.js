c79c607be2668f00a746e86df7247d19ed780633
santiago ortiz <santimaria0623@gmail.com>
2020-05-06 16:34:49 -0500

    añadiendo las fotos al banner y poniendo un nuevo contenedor para una nueva imagen

--- JavaScript/img.js
+++ JavaScript/img.js
@@ -2,14 +2,15 @@ const imgOne   = document.getElementById('mini_one');
 const imgTwo   = document.getElementById('mini_two');
 const imgThree = document.getElementById('mini_three');
 const imgFour  = document.getElementById('mini_four');
+const imgFive = document.getElementById('mini_five');
 
-const arrayImg = ['../img-banner/uno.jpg'   ,
-                  '../img-banner/dos.jpg'   ,
-                  '../img-banner/tres.jpg'  ,
-                  '../img-banner/cuatro.jpg',
-                  '../img-banner/cinco.jpg' ,
-                  '../img-banner/seis.jpg'  , 
-                  '../img-banner/siete.jpg'  ];
+const arrayImg = ['../img-banner/image1.jpeg',
+                  '../img-banner/image2.jpeg',
+                  '../img-banner/image3.jpeg',
+                  '../img-banner/image4.jpeg',
+                  '../img-banner/image5.jpeg',
+                  '../img-banner/image6.jpeg', 
+                  '../img-banner/image7.jpeg'];
 
 class Imagen {
         constructor(name){
@@ -18,13 +19,15 @@ class Imagen {
         }
         RandomImg () {
                 this.name.src = arrayImg[  Math.floor(Math.random()*7)  ];
-                console.log(`Esta es la primera dirreción ${this.name.src}`)
+                //parce deberias quitar esos console.log porque va a saturar la pagina
+                console.log(`Esta es la primera dirreción ${this.name.src}`);
         }
 }
 function evento (){
         setInterval ( () =>
-        {this.name.src = arrayImg[  Math.floor(Math.random()*7)  ]
-         console.log(`Esta es la segunda dirección ${this.name.src}`);}, 3000 )
+        {this.name.src = arrayImg[  Math.floor(Math.random()*7)  ];
+                //parce deberias quitar esos console.log porque va a saturar la pagina
+         console.log(`Esta es la segunda dirección ${this.name.src}`);}, 3000 );
 }
 let squareOne   = new Imagen (imgOne);
 imgOne.addEventListener('load', evento.call(squareOne));
@@ -34,5 +37,7 @@ let squareThree = new Imagen (imgThree);
 imgThree.addEventListener('load', evento.call(squareThree));
 let squareFour  = new Imagen (imgFour);
 imgFour.addEventListener('load', evento.call(squareFour));
+let squareFive = new Imagen (imgFive);
+imgFive.addEventListener('load', evento.call(squareFive));
 
 
