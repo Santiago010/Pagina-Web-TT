1e7219fff4d75ceef2d28435d870bd87fe331b44
santiago ortiz <Santimaria0623@gmail.com>
2020-05-17 18:35:46 -0500

    configurando productos

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,12 +1,12 @@
 const imgOne = document.getElementById('mini_one');
 
-const arrayImg = ['../docs/image1.jpeg',
-                  '../docs/image2.jpeg',
-                  '../docs/image3.jpeg',
-                  '../docs/image4.jpeg',
-                  '../docs/image5.jpeg',
-                  '../docs/image6.jpeg', 
-                  '../docs/image7.jpeg'];
+const arrayImg = [
+        '../docs/imagenesIndex/image1.jpeg',
+        '../docs/imagenesIndex/image2.jpeg',
+        '../docs/imagenesIndex/image3.jpeg',
+        '../docs/imagenesIndex/image4.jpeg',
+        '../docs/imagenesIndex/image5.jpeg'
+];
 class Imagen {
         constructor(name){
                 this.name = name;
@@ -15,11 +15,11 @@ class Imagen {
                 this.name.addEventListener('load', this.changeSource);
         }
         randomImg () {
-                this.name.src = arrayImg[  Math.floor(Math.random()*7)  ];
+                this.name.src = arrayImg[  Math.floor(Math.random()*5)  ];
         }
         changeSource (){
                 setTimeout ( () =>
-                { this.name.src = arrayImg[  Math.floor(Math.random()*7)  ] }, 3000 );
+                { this.name.src = arrayImg[  Math.floor(Math.random()*5)  ] }, 3000 );
         }
 }
 const squareOne   = new Imagen (imgOne);
--- JavaScript/products.js
+++ JavaScript/products.js
@@ -1,3 +1,43 @@
+//var para llamar a los botones
+var boton1 = document.getElementById('boton1');
+var boton2 = document.getElementById('boton2');
+var boton3 = document.getElementById('boton3');
+var boton4 = document.getElementById('boton4');
+var boton5 = document.getElementById('boton5');
+var boton6 = document.getElementById('boton6');
+var boton7 = document.getElementById('boton7');
+
+//var para llamar a los elementos del carousel
+var descripcionC = document.getElementById('description');
+var tituloProductosC = document.getElementById('tituloProducto');
+var subtituloProductosC = document.getElementById('subtituloProducto');
+var imagen1C = document.getElementById('1imagenC');
+var imagen2C = document.getElementById('2imagenC');
+var imagen3C = document.getElementById('3imagenC');
+
+boton1 = document.addEventListener("click", function(){
+        console.log("se esta ejecutando la funcion del boton cunas");
+        descripcionC.textContent = "en estas imagenes se puede observar unas pequeñas muestras del trabajo que realizamos con cunas";
+        subtituloProductosC.textContent = "Productos";
+        tituloProductosC.textContent = "Cunas";
+        imagen1C.textContent = '../docs/imagenesProducts/cunas/imgC1.jpg';
+        imagen2C.textContent = '../docs/imagenesProducts/cunas/imgC2.jpg';
+        imagen3C.textContent = '../docs/imagenesProducts/cunas/imgC3.jpg';
+});
+
+
+boton2 = document.addEventListener("click", function(){
+        console.log("se esta ejecutando la funcion del boton exhibidores");
+        descripcionC.textContent = "en estas imagenes se puede observa unas pequeñas muestras del trabajo que realizamos con exhibidores";
+        subtituloProductosC.textContent = "Productos";
+        tituloProductosC.textContent = "Exhibidores";
+        imagen1C.textContent = '../docs/imagenesProducts/exhibidores/imgE1.jpg';
+        imagen2C.textContent = '../docs/image2.jpeg';
+        imagen3C.textContent = '../docs/image3.jpeg';
+});
+
+
+
 // CAROUSEL
 document.addEventListener('DOMContentLoaded', () => {
         const elementosCarousel = document.querySelectorAll('.carousel');
