937c1b9294e913c4855d6e9009883ac7a112276b
santiago ortiz <Santimaria0623@gmail.com>
2020-05-18 17:07:37 -0500

    ultimos cambios solo falta el ressponsive en productos y quienes somos

--- JavaScript/products.js
+++ JavaScript/products.js
@@ -1,90 +1,3 @@
-// //var para llamar a los botones
-// var boton1 = document.getElementById('boton1');
-// var boton2 = document.getElementById('boton2');
-// var boton3 = document.getElementById('boton3');
-// var boton4 = document.getElementById('boton4');
-// var boton5 = document.getElementById('boton5');
-// var boton6 = document.getElementById('boton6');
-// var boton7 = document.getElementById('boton7');
-
-// //var para llamar a los elementos del carousel
-// var descripcionC = document.getElementById('description');
-// var tituloProductosC = document.getElementById('tituloProducto');
-// var subtituloProductosC = document.getElementById('subtituloProducto');
-// var imagen1C = document.getElementById('1imagenC');
-// var imagen2C = document.getElementById('2imagenC');
-// var imagen3C = document.getElementById('3imagenC');
-
-// boton1.addEventListener("click", function(){
-//         console.log("se esta ejecutando la funcion del boton cunas");
-//         descripcionC.textContent = "en estas imagenes se puede observar unas pequeñas muestras del trabajo que realizamos con cunas";
-//         subtituloProductosC.textContent = "Productos";
-//         tituloProductosC.textContent = "Cunas";
-//         imagen1C.src = '../docs/imagenesProducts/cunas/imgC1.jpg';
-//         imagen2C.src = '../docs/imagenesProducts/cunas/imgC2.jpg';
-//         imagen3C.src = '../docs/imagenesProducts/cunas/imgC3.jpg';
-// });
-
-
-// boton2.addEventListener("click", function(){
-//         console.log("se esta ejecutando la funcion del boton exhibidores");
-//         descripcionC.textContent = "en estas imagenes se puede observa unas pequeñas muestras del trabajo que realizamos con exhibidores";
-//         subtituloProductosC.textContent = "Productos";
-//         tituloProductosC.textContent = "Exhibidores";
-//         imagen1C.src = '../docs/imagenesProducts/exhibidores/imgE1.jpg';
-//         imagen2C.src = '../docs/imagenesProducts/exhibidores/imgE1.jpg';
-//         imagen3C.src = '../docs/imagenesProducts/exhibidores/imgE1.jpg';
-// });
-
-// boton3.addEventListener("click",function(){
-//         console.log("se esta ejecutando la funcion del boton Probadores");
-//         descripcionC.textContent = "en estas imagenes se puede observa unas pequeñas muestras del trabajo que realizamos con Probadores";
-//         subtituloProductosC.textContent = "Productos";
-//         tituloProductosC.textContent = "Probadores";
-//         imagen1C.src = '../docs/imagenesProducts/probadores/imgp1.jpg';
-//         imagen2C.src = '../docs/imagenesProducts/probadores/imgp2.jpg';
-//         imagen3C.src = '../docs/imagenesProducts/probadores/imgp3.jpg';  
-// });
-
-// boton4.addEventListener("click", function(){
-//         console.log("se esta ejecutando la funcion del boton Estuches");
-//         descripcionC.textContent = "en estas imagenes se puede observa unas pequeñas muestras del trabajo que realizamos con Estuches";
-//         subtituloProductosC.textContent = "Productos";
-//         tituloProductosC.textContent = "Estuches";
-//         imagen1C.src = '../docs/imagenesProducts/estuches/imgE1.jpg';
-//         imagen2C.src = '../docs/imagenesProducts/estuches/imgE2.jpg';
-//         imagen3C.src = '../docs/imagenesProducts/estuches/imgE1.jpg'; 
-// });
-
-// boton5.addEventListener("click", function(){
-//         console.log("se esta ejecutando la funcion del boton Empaques Blister");
-//         descripcionC.textContent = "en estas imagenes se puede observa unas pequeñas muestras del trabajo que realizamos con Empaques Blister";
-//         subtituloProductosC.textContent = "Productos";
-//         tituloProductosC.textContent = "Blister";
-//         imagen1C.src = '../docs/imagenesProducts/empaquesBlister/';
-//         imagen2C.src = '../docs/imagenesProducts/empaquesBlister/';
-//         imagen3C.src = '../docs/imagenesProducts/empaquesBlister/';   
-// });
-
-// boton6.addEventListener("click", function(){
-//         console.log("se esta ejecutando la funcion del boton Liners Acetatos");
-//         descripcionC.textContent = "en estas imagenes se puede observa unas pequeñas muestras del trabajo que realizamos con Liners Acetatos";
-//         subtituloProductosC.textContent = "Productos";
-//         tituloProductosC.textContent = "Liners & Acetatos";
-//         imagen1C.src = '../docs/imagenesProducts/acetatosLiners/';
-//         imagen2C.src = '../docs/imagenesProducts/acetatosLiners/';
-//         imagen3C.src = '../docs/imagenesProducts/acetatosLiners/'; 
-// });
-
-// boton7.addEventListener("click", function(){
-//         console.log("se esta ejecutando la funcion del boton Moldes");
-//         descripcionC.textContent = "en estas imagenes se puede observa unas pequeñas muestras del trabajo que realizamos con moldes";
-//         subtituloProductosC.textContent = "Productos";
-//         tituloProductosC.textContent = "Moldes";
-//         imagen1C.src = '../docs/imagenesProducts/moldes/imagen1p.jpg';
-//         imagen2C.src = '../docs/imagenesProducts/moldes/imagen2p.jpg';
-//         imagen3C.src = '../docs/imagenesProducts/moldes/imagen1p.jpg';    
-// });
 
 
 // CAROUSEL
@@ -116,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
                                 'En estas imágenes se puede observar unas pequeñas muestras del trabajo que realizamos con empaques blister',
                                 'En estas imágenes se puede observar unas pequeñas muestras del trabajo que realizamos con moldes'            ];     
 
-        const tituloPrincipal = [ 'CUNAS', 'EXHIBIDORES', 'PROBADORES', 'ESTUCHES', 'EMPAQUES BLISTER', 'MOLDES' ];  
+        const tituloPrincipal = [ 'Cunas', 'Exhibidores', 'Probadores', 'Estuches', 'Empaques Blister', 'Moldes' ];  
 
         const URL   = '../docs/imagenesProducts/';
         const rutas = [
@@ -126,17 +39,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
         /* EXHIBIDORES */       [ `${URL}exhibidores/imgE1.jpg` ],
 
-        /* PROBADORES */        [ `${URL}probadores/imagen3p.jpg` , 
-                                  `${URL}probadores/imagen11p.jpg`, 
-                                  `${URL}probadores/imagen12p.jpg` ],
+        /* PROBADORES */        [ `${URL}probadores/imgP1.jpg` , 
+                                  `${URL}probadores/imgP2.jpg`, 
+                                  `${URL}probadores/imgP3.jpg` ],
 
-        /* ESTUCHES */          [ `${URL}estuches/imagen4p.jpg`, 
-                                  `${URL}estuches/imagen10p.jpg` ],
+        /* ESTUCHES */          [ `${URL}estuches/imgE1.jpg`, 
+                                  `${URL}estuches/imgE2.jpg` ],
 
         /* EMPAQUES BLISTER */  [],
 
-        /* MOLDES */            [`${URL}moldes/imagen1p.jpg`, 
-                                 `${URL}moldes/imagen2p.jpg`]
+        /* MOLDES */            [`${URL}moldes/imagen1P.jpg`, 
+                                 `${URL}moldes/imagen2P.jpg`,
+                                 `${URL}moldes/imagen3P.jpg`,
+                                ]
         ]
         
         botones.forEach( (elemento, index) => elemento.addEventListener('click', ()=>{
