82b6b09248c74b23b593b545ef967496a034282b
santiago010 <santimaria0623@gmail.com>
2020-07-07 11:51:42 -0500

    haciendo pruebas

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -2,31 +2,33 @@ import "./API_FACEBOOK.js";
 
 const imgOne = document.getElementById("mini_one");
 
-const arrayImg = [
-  "../docs/imagenesIndex/imgB1.jpg",
-  "../docs/imagenesIndex/imgB2.jpg",
-  "../docs/imagenesIndex/imgB3.jpg",
-  "../docs/imagenesIndex/imgB4.jpg",
-];
+imgOne.src = "../docs/imagenesIndex/imgB1.jpg"
 
-class Imagen {
-  constructor(name) {
-    this.name = name;
-    this.randomImg();
-    this.changeSource = this.changeSource.bind(this);
-    this.name.addEventListener("load", this.changeSource);
-  }
-  randomImg() {
-    this.name.src = arrayImg[Math.floor(Math.random() * 4)];
-  }
-  changeSource() {
-    setTimeout(() => {
-      this.name.src = arrayImg[Math.floor(Math.random() * 4)];
-    }, 2000);
-  }
-}
-const squareOne = new Imagen(imgOne);
+// const arrayImg = [
+//   "../docs/imagenesIndex/imgB1.jpg",
+//   "../docs/imagenesIndex/imgB2.jpg",
+//   "../docs/imagenesIndex/imgB3.jpg",
+//   "../docs/imagenesIndex/imgB4.jpg",
+// ];
 
-// if ("serviceWorker" in navigator) {
-//   navigator.serviceWorker.register("../sw.js")
+// class Imagen {
+//   constructor(name) {
+//     this.name = name;
+//     this.randomImg();
+//     this.changeSource = this.changeSource.bind(this);
+//     this.name.addEventListener("load", this.changeSource);
+//   }
+//   randomImg() {
+//     this.name.src = arrayImg[Math.floor(Math.random() * 4)];
+//   }
+//   changeSource() {
+//     setTimeout(() => {
+//       this.name.src = arrayImg[Math.floor(Math.random() * 4)];
+//     }, 2000);
+//   }
 // }
+// const squareOne = new Imagen(imgOne);
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("../sw.js").then(event => console.log(event))
+}
