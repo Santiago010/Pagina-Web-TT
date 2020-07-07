cc75f4a77fbc2703430223fd9141c4d516baf2f1
santiago010 <santimaria0623@gmail.com>
2020-07-07 12:17:15 -0500

    posible solucion al problema de inicio

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -2,33 +2,31 @@ import "./API_FACEBOOK.js";
 
 const imgOne = document.getElementById("mini_one");
 
-imgOne.src = "../docs/imagenesIndex/imgB1.jpg"
+const arrayImg = [
+  "../docs/imagenesIndex/imgB1.jpg",
+  "../docs/imagenesIndex/imgB2.jpg",
+  "../docs/imagenesIndex/imgB3.jpg",
+  "../docs/imagenesIndex/imgB4.jpg",
+];
 
-// const arrayImg = [
-//   "../docs/imagenesIndex/imgB1.jpg",
-//   "../docs/imagenesIndex/imgB2.jpg",
-//   "../docs/imagenesIndex/imgB3.jpg",
-//   "../docs/imagenesIndex/imgB4.jpg",
-// ];
-
-// class Imagen {
-//   constructor(name) {
-//     this.name = name;
-//     this.randomImg();
-//     this.changeSource = this.changeSource.bind(this);
-//     this.name.addEventListener("load", this.changeSource);
-//   }
-//   randomImg() {
-//     this.name.src = arrayImg[Math.floor(Math.random() * 4)];
-//   }
-//   changeSource() {
-//     setTimeout(() => {
-//       this.name.src = arrayImg[Math.floor(Math.random() * 4)];
-//     }, 2000);
-//   }
-// }
+class Imagen {
+  constructor() {
+    this.name = imgOne;
+    this.randomImg()
+    this.changeSource = this.changeSource.bind(this);
+    this.name.addEventListener("load", this.changeSource);
+  }
+  randomImg() {
+    this.name.src = arrayImg[Math.floor(Math.random() * 4)];
+  }
+  changeSource() {
+    setTimeout(() => {
+      this.name.src = arrayImg[Math.floor(Math.random() * 4)];
+    }, 2000);
+  }
+}
 // const squareOne = new Imagen(imgOne);
 
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("../sw.js").then(event => console.log(event))
+  navigator.serviceWorker.register("../sw.js").then(event => console.log(event)).catch(error => console.log(error))
 }
