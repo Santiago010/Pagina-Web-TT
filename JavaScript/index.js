8c9e8525bb3cd4ec4587e8e9b1214a522e117dfb
santiago010 <santimaria0623@gmail.com>
2020-07-06 22:51:55 -0500

    pagina terminada

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -28,8 +28,5 @@ class Imagen {
 const squareOne = new Imagen(imgOne);
 
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("../sw.js")
-    // .then((event) => console.log(event))
-    .catch((Error) => console.log(Error.message));
+  navigator.serviceWorker.register("../sw.js")
 }
